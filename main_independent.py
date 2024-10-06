dc12e51ffb8d6227d2e480115b8a5e155dc14f0a
Hhhzj-7 <63787460+Hhhzj-7@users.noreply.github.com>
2024-10-06 21:01:19 +0800

    Update main_independent.py

--- main_independent.py
+++ main_independent.py
@@ -1,6 +1,8 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
-from data import RNA_dataset, Molecule_dataset
+import os
+import torch
+from data import RNA_dataset, Molecule_dataset, RNA_dataset_independent, Molecule_dataset_independent, WordVocab
 from model import RNA_feature_extraction, GNN_molecule, mole_seq_model, cross_attention
 from torch_geometric.loader import DataLoader
 import torch.optim as optim
@@ -9,38 +11,25 @@ from torch.autograd import Variable
 import numpy as np
 import os
 import torch.nn as nn
-from sklearn.model_selection import RepeatedStratifiedKFold
-from sklearn.preprocessing import KBinsDiscretizer
 from sklearn.metrics import mean_squared_error
 import random
-import pandas as pd
+torch.set_printoptions(profile="full")
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+hidden_dim = 16
 
-BATCH_SIZE = 16
-EPOCH = 1500
-hidden_dim = 128
-seed_dataset = 2
-
-# blind rna: rna; blind mole: mole; blind both: rm
-cold_type = 'rna'
-seed = 2 
-LR = 5e-4
-
-RNA_type = 'All_sf'
+EPOCH = 200
+RNA_type = 'Viral_RNA_independent'
 rna_dataset = RNA_dataset(RNA_type)
 molecule_dataset = Molecule_dataset(RNA_type)
 
-all_df = pd.read_csv('data/RSM_data/' + 'All_sf' + '_dataset_v1.csv', delimiter='\t')  
+rna_dataset_in = RNA_dataset_independent()
+molecule_dataset_in = Molecule_dataset_independent()
 
-df1 = pd.read_csv('data/blind_test/cold_' + cold_type +'1.csv', delimiter=',')
-df2 = pd.read_csv('data/blind_test/cold_' + cold_type +'2.csv', delimiter=',')
-df3 = pd.read_csv('data/blind_test/cold_' + cold_type +'3.csv', delimiter=',')
-df4 = pd.read_csv('data/blind_test/cold_' + cold_type +'4.csv', delimiter=',')
-df5 = pd.read_csv('data/blind_test/cold_' + cold_type +'5.csv', delimiter=',')
-df = [df1, df2, df3, df4, df5]
-df_cold_all = pd.concat([df1, df2,df3, df4,df5], axis=0).reset_index()
+seed = 1
+
+
 
 # set random seed
 def set_seed(seed):
@@ -68,6 +57,14 @@ class CustomDualDataset(Dataset):
 
     def __len__(self):
         return len(self.dataset1)  
+
+
+
+def average_multiple_lists(lists):
+    return [sum(item)/len(lists) for item in zip(*lists)]
+
+
+
 
 
 # DeepRSMA architecture
@@ -154,94 +151,68 @@ class DeepRSMA(nn.Module):
         return out
 
 
-fold = 0
-p_list = []
-s_list = []
-r_list = []
-for i, df_f in enumerate(df):
-   
-    test_id = df_f['Entry_ID'].tolist()
-    test_id = all_df[all_df['Entry_ID'].isin(test_id)].index.tolist()
-    
-    train_id = []
-    for j, other_id in enumerate(df):
-        if j != i:
-            train_id.extend(other_id['Entry_ID'].tolist())
-    
-    train_id = all_df[all_df['Entry_ID'].isin(train_id)].index.tolist()
-    
-    print(len(test_id), len(train_id))
+# use viral RNA to train
+train_dataset = CustomDualDataset(rna_dataset, molecule_dataset)
+# independent test
+test_dataset = CustomDualDataset(rna_dataset_in, molecule_dataset_in)
 
-    max_p = -1
-    max_s = 0
-    max_rmse = 0
 
-    fold = fold + 1
-    print("Fold", fold)
+train_loader = DataLoader(
+    train_dataset, batch_size=8, num_workers=1, drop_last=False, shuffle=False
+)
+test_loader = DataLoader(
+    test_dataset, batch_size=1, num_workers=1, drop_last=False, shuffle=False
+)
 
-    
-    train_dataset = CustomDualDataset(rna_dataset[train_id], molecule_dataset[train_id])
-    test_dataset = CustomDualDataset(rna_dataset[test_id], molecule_dataset[test_id])
 
-    
-    train_loader = DataLoader(
-        train_dataset, batch_size=BATCH_SIZE, num_workers=1, drop_last=False, shuffle=False
-    )
 
-    test_loader = DataLoader(
-        test_dataset, batch_size=BATCH_SIZE, num_workers=1, drop_last=False, shuffle=False
-    )
-    
-    
-    model = DeepRSMA().to(device)
-    
-    
-    optimizer = optim.Adam(model.parameters(), lr=LR, weight_decay=1e-7)
-    optimal_loss = 1e10
-    loss_fct = torch.nn.MSELoss()
-    for epo in range(EPOCH):
-        # train
-        train_loss = 0
-        for step, (batch_rna,batch_mole) in enumerate(train_loader):
-            optimizer.zero_grad()
-            pre = model(batch_rna.to(device), batch_mole.to(device))
-            loss = loss_fct(pre.squeeze(dim=1), batch_rna.y)
-            loss.backward()
-            optimizer.step()
-            train_loss = train_loss + loss
-        # test
-        with torch.set_grad_enabled(False):
-            test_loss = 0
-            model.eval()
-            y_label = []
-            y_pred = []
-            for step, (batch_rna_test,batch_mole_test) in enumerate(test_loader):
-                label = Variable(torch.from_numpy(np.array(batch_rna_test.y))).float()
-                score = model(batch_rna_test.to(device), batch_mole_test.to(device))
-                n = torch.squeeze(score, 1)
-                logits = torch.squeeze(score).detach().cpu().numpy()
-                label_ids = label.to('cpu').numpy()
-                loss_t = loss_fct(n.cpu(), label)
-                y_label = y_label + label_ids.flatten().tolist()
-                y_pred = y_pred + logits.flatten().tolist()
-                test_loss = test_loss + loss_t
-            
-        model.train()
+model = DeepRSMA()
+model.to(device)
+
+y_pred_all = []
+max_p = -1
+
+optimizer = optim.Adam(model.parameters(), lr=6e-5 , weight_decay=1e-5)
+optimal_loss = 1e10
+loss_fct = torch.nn.MSELoss()
+for epoch in range(0,EPOCH):
+    train_loss = 0
+
+    for step, batch in enumerate(train_loader):
+        optimizer.zero_grad()
+        pre = model(batch[0].to(device), batch[1].to(device))
+
+        y = batch[0].y
+        
+        loss = loss_fct(pre.squeeze(dim=1), y.float())
+        loss.backward()
+        optimizer.step()
+        train_loss = train_loss + loss
+    with torch.set_grad_enabled(False):
+        model.eval()
+        y_label = []
+        y_pred = []
+        for step, (batch_v) in enumerate(test_loader):
+            label = Variable(torch.from_numpy(np.array(batch_v[0].y))).float()
+            score = model(batch_v[0].to(device), batch_v[1].to(device))
+
+            logits = torch.squeeze(score).detach().cpu().numpy()
+            label_ids = label.to('cpu').numpy()
+
+            y_label = y_label + label_ids.flatten().tolist()
+            y_pred = y_pred + logits.flatten().tolist()
+
         p = pearsonr(y_label, y_pred)
         s = spearmanr(y_label, y_pred)
         rmse = np.sqrt(mean_squared_error(y_label, y_pred))
+        print( 'epo:',epoch, 'pcc:',p[0],'scc: ',s[0], 'rmse:',rmse)
+
         if max_p < p[0]:
-            print('epo:',epo, 'pcc:',p[0],'scc: ',s[0], 'rmse:',rmse)
             max_p = p[0]
-            max_s = s[0]
-            max_rmse =rmse
-            torch.save(model.state_dict(), 'save/' + 'model_blind_'+str(cold_type)+str(seed_dataset)+'_'+str(fold)+'_'+str(seed)+'.pth')
-    
-    p_list.append(max_p)
-    r_list.append(max_rmse)
-    s_list.append(max_s)
+            print(' ')
+            print('Best:', 'epo:',epoch, 'pcc:',p[0],'scc: ',s[0],'rmse:',rmse)
 
-    print('p:', np.mean(p_list))
-    print('s:', np.mean(s_list))
-    print('rmse:', np.mean(r_list))
+            torch.save(model.state_dict(), 'save/' + 'model_independent_'+str(seed)+'.pth')
 
+        
+        model.train()
